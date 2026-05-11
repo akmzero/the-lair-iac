2dcbfad41858af14882c9f58d91f6c8829199237
akmzero <akmzero@gmail.com>
2026-05-11 15:52:37 -0400

    added main.tf

---
+++ terraform/proxmox/first-vm/main.tf
@@ -0,0 +1,19 @@
+resource "proxmox_vm_qemu" "test_vm" {
+  name        = "tf-ubuntu-base"
+  target_node = "pve"
+  clone       = "ubuntu-cloud-template"
+  full_clone  = true
+
+  cores   = 2
+  memory  = 2048
+
+  agent = 1
+
+  os_type = "cloud-init"
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+}
--- terraform/proxmox/first-vm/variables.tf
+++ terraform/proxmox/first-vm/variables.tf
@@ -1,6 +1,6 @@
 variable "proxmox_api_url" {
   type        = string
-  description = "The URL of the Proxmox API endpoint, e.g. https://10.5.0.5:8006/api2/json"
+  description = "The URL of the Proxmox API endpoint"
 }
 
 variable "proxmox_api_token_id" {
